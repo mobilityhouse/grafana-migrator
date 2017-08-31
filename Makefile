097d5b5de9cf73e615023e2231a5524ec4458c4a
David Asabina <vid@bina.me>
2017-08-31 12:57:49 +0200

    connection to the API

--- Makefile
+++ Makefile
@@ -8,12 +8,15 @@ image:
 shell:
 	${DOCKER} run --rm -it  \
 		-v ${PWD}/data/secret:/tmp/secret \
+		-v ${PWD}/data/setup.yaml:/tmp/setup.yaml \
 		-v ${PWD}/src:/bench \
 		${TAG} \
 		/bin/bash
 
 build:
 	${DOCKER} run --rm -it  \
+		-v ${PWD}/data/secret:/tmp/secret \
+		-v ${PWD}/data/setup.yaml:/tmp/setup.yaml \
 		-v ${PWD}/src:/bench \
 		${TAG} \
 		python config.py
--- src/config.py
+++ src/config.py
@@ -1,3 +1,27 @@
 import http.client, csv , json, re, yaml
 
-print("things could be easier")
+def retrieve_secret():
+  with open("/tmp/secret", "r") as f:
+    return f.read()
+
+def retrieve_config():
+  with open("/tmp/setup.yaml", "r") as f:
+    return yaml.load(f)
+
+config = retrieve_config()
+secret = config["secret"] # retrieve_secret()
+
+print(f"things could be easier {secret}")
+print(config)
+
+conn = http.client.HTTPConnection(
+    host = config["host"], port = config["port"]
+    )
+
+auth_headers = { "Authorization": f"Bearer {secret}" }
+print(auth_headers)
+
+conn.request("GET", config["endpoint"], headers = auth_headers)
+r1 = conn.getresponse()
+
+print(f"status: {r1.status}, reason: {r1.reason}, data {r1.read()}")
