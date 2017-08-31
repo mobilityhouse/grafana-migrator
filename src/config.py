39c63c712d7b8033c20fb840254e60ceaf0c6f54
David Asabina <vid@bina.me>
2017-08-31 16:01:48 +0200

    api call tested

--- src/config.py
+++ src/config.py
@@ -19,18 +19,22 @@ conn = http.client.HTTPConnection(
     host = config["host"], port = config["port"]
     )
 
-auth_headers = { "Authorization": f"Bearer {secret}" }
-print(auth_headers)
+headers = {
+    "Authorization": f"Bearer {secret}",
+    "Content-Type": "application/json",
+    "Accept": "application/json",
+    }
 
 for filename in os.listdir("/tmp/out"):
   with open(f"/tmp/out/{filename}", "r") as source:
     config = json.load(source)
-    title = "AUTO " + config["title"]
+    title = config["title"]
+    config["title"] = title
     config.pop("id", None) # remove the id just to be safe
     data = { "dashboard": config, "overwrite": True }
     payload = json.dumps(data)
     print(f"Updating: {title}")
-#    conn.request("POST", "/api/dashboards/db", headers = auth_headers, body = payload)
-#    #print(json.dumps(data, indent=2))
-#    res = conn.getresponse()
-#    print(f"status: {res.status}, reason: {res.reason}, data {res.read()}")
+    conn.request("POST", "/api/dashboards/db", headers = headers, body = payload)
+    #print(json.dumps(data, indent=2))
+    res = conn.getresponse()
+    print(f"status: {res.status}, reason: {res.reason}, data {res.read()}")
