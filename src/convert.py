e834d6687e4082bc4115e7085f2ab6550006f51a
David Asabina <vid@bina.me>
2017-08-31 14:19:02 +0200

    :rocket:

---
+++ src/convert.py
@@ -0,0 +1,47 @@
+import os
+import json
+
+def retrieve_input(filename):
+  with open(filename, "r") as f:
+    return json.load(f)
+
+def produce_output(filename):
+  with open(filename, "w") as f:
+    return json.dump(content, filename, indent=2)
+
+#def convert_data_row(row):
+#  print(row)
+#  return row
+
+def convert_field(value):
+  if value.startswith("@"):
+    return value
+  else:
+    return f"message.{value}"
+
+def convert_metrics(metric):
+  metric["field"] = convert_field(metric["field"])
+  return metric
+
+def convert_target(target):
+  target["metrics"] = list(map(convert_metrics, target["metrics"]))
+  return target
+
+def convert_panel(panel):
+  panel["targets"] = list(map(convert_target, panel["targets"]))
+  return panel
+
+def convert_row(row):
+  row["panels"] = list(map(convert_panel, row["panels"]))
+  return row
+
+def convert(data):
+  data["rows"] = list(map(convert_row, data["rows"]))
+  return data
+
+# targets[metrics.field] currentSOC -> message.currentSOC
+# metrics
+for filename in os.listdir("/tmp/in"):
+  with open(f"/tmp/out/{filename}", "w") as destination:
+    data = convert(retrieve_input(f"/tmp/in/{filename}"))
+    json.dump(data, destination, indent=2)
